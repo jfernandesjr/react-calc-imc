e50fe7bde4b3d8930438070ea9cb8b9393432ecd
José Fernandes <josefjunior4@hotmail.com>
2023-03-23 11:37:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Calculadora de IMC (Versão em Português)
+
+Projeto em React + Typescript fazendo uma calculadora de IMC.
+
+Projeto desenvolvido durante o curso [B7Web](https://b7web.com.br).
+
+### Instalação
+- `npm install`
+
+### Para rodar
+- `npm start`
+
+<br><br>
+
+
+# BMI Calculator (English Version)
+
+React + Typescript project creating a BMI calculator.
+
+Project developed during the [B7Web](https://b7web.com.br) course.
+
+### To install
+- `npm install`
+
+### To run
+- `npm start`
---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
+import { GridItem } from './components/GridItem';
+
+import { levels, calculateImc, Level } from './helpers/imc'
+
+function App() {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Digite todos os campos.");
+    }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  }
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p> IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.
+            O índice é calculado da seguinte maneira: divide-se o peso do paciente pela
+            sua altura elevada ao quadrado. Diz-se que o indivíduo tem peso normal quando
+            o resultado do IMC está entre 18,5 e 24,9. <br /><br />Quer descobrir seu IMC? Insira
+            seu peso e sua altura nos campos abaixo e clique em "calcular".</p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura em metros. (Ex: 1.53) "
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+          <input
+            type="number"
+            placeholder="Digite o seu peso em kg. (Ex: 74.8) "
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+
+          <button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
+          
+        </div>
+        <div className={styles.rightSide}>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          }
+          {toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
